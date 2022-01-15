3bcfb29bde985d36ec9afde7f537eb96e6206a01
r7l <30982145+r7l@users.noreply.github.com>
2022-01-16 00:07:55 +0100

    initial commit

---
+++ gitea_api/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from gitea_api.api.admin_api import AdminApi
+from gitea_api.api.issue_api import IssueApi
+from gitea_api.api.miscellaneous_api import MiscellaneousApi
+from gitea_api.api.notification_api import NotificationApi
+from gitea_api.api.organization_api import OrganizationApi
+from gitea_api.api.repository_api import RepositoryApi
+from gitea_api.api.settings_api import SettingsApi
+from gitea_api.api.user_api import UserApi
